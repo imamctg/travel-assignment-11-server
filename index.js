e4223c401d07317473006c5e7c59632385675ec2
imamctg <im788788@gmail.com>
2021-11-13 16:56:57 +0600

    first commit

--- index.js
+++ index.js
@@ -16,108 +16,158 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const database = client.db('travelling');
-        const serviceCollection = database.collection('services');
-        const hotelCollection = database.collection('hotels');
-        const bookingCollection = database.collection('bookings');
+        const database = client.db('carSeller');
+        const productCollection = database.collection('products');
+        const reviewCollection = database.collection('reviews');
+        const orderCollection = database.collection('orders');
+        const usersCollection = database.collection('users');
 
         // GET PRODUCTS API 
-        app.get('/services', async (req, res) => {
-            const cursor = serviceCollection.find({});
-            const services = await cursor.toArray();
-            res.send(services);
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
         })
 
-        // GET PRODUCTS API 
-        app.get('/hotels', async (req, res) => {
-            const cursor = hotelCollection.find({});
-            const hotels = await cursor.toArray();
-            res.send(hotels);
-        })
 
-        // GET SINGLE SERVICE
-        app.get('/services/:id', async (req, res) => {
+
+        // GET SINGLE PRODUCT
+        app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             console.log('getting specific service', id)
             const query = { _id: ObjectId(id) };
-            const service = await serviceCollection.findOne(query);
-            res.json(service);
+            const product = await productCollection.findOne(query);
+            res.json(product);
         })
-        // GET SINGLE SERVICE
-        app.get('/bookings/:id', async (req, res) => {
+        // GET SINGLE ORDER
+        app.get('/orders/:id', async (req, res) => {
             const id = req.params.id;
             console.log('getting specific service', id)
             const query = { _id: ObjectId(id) };
-            const bookings = await bookingCollection.findOne(query);
-            res.json(bookings);
+            const orders = await orderCollection.findOne(query);
+            res.json(orders);
         })
 
         // POST API
-        app.post('/services', async (req, res) => {
-            const service = req.body;
-            console.log('hit the post api', service)
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            console.log('hit the post api', product)
             console.log('Post Hitted')
-            const result = await serviceCollection.insertOne(service);
+            const result = await productCollection.insertOne(product);
+            console.log(result)
+            res.send(result)
+        })
+        // POST API REVIEWS
+        app.post('/reviews', async (req, res) => {
+            const reviews = req.body;
+            console.log('hit the post api', reviews)
+            console.log('Post Hitted')
+            const result = await reviewCollection.insertOne(reviews);
             console.log(result)
             res.send(result)
         })
 
-        // ADD BOOKING API
-        app.post('/bookings', async (req, res) => {
-            const bookings = req.body;
-            const result = await bookingCollection.insertOne(bookings);
+        // GET REVIEW API 
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        // ADD ORDER API
+        app.post('/orders', async (req, res) => {
+            const orders = req.body;
+            const result = await orderCollection.insertOne(orders);
             res.json(result);
         })
 
 
 
-        // GET BOOKING
+        // GET ALL ORDER
 
-        app.get('/bookings', async (req, res) => {
-            const result = await bookingCollection.find({}).toArray();
+        app.get('/orders', async (req, res) => {
+            const result = await orderCollection.find({}).toArray();
             res.send(result);
 
         });
 
-        // UPDATE API
-        app.put('/bookings/:id', async (req, res) => {
+        // UPDATE ORDER
+        app.put('/order/:id', async (req, res) => {
             console.log(req)
             const id = req.params.id;
             const orderStatus = req.body;
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
-            const bookings = {
+            const orders = {
                 $set: {
                     status: orderStatus.status
                 }
             }
 
-            const result = await bookingCollection.updateOne(filter, bookings, options)
+            const result = await orderCollection.updateOne(filter, orders, options)
+            console.log(result);
             res.json(result)
-            console.log('Updating User', req);
-            res.json('Updating not dating')
+
 
 
         })
 
         // DELETE API
-        app.delete('/bookings/:id', async (req, res) => {
+        app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             console.log('checking', id);
             const query = { _id: ObjectId(id) };
-            const result = await bookingCollection.deleteOne(query);
+            const result = await orderCollection.deleteOne(query);
             res.json(result);
         })
 
 
 
         // DELETE API
-        app.delete('/services/:id', async (req, res) => {
+        app.delete('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await serviceCollection.deleteOne(query);
+            const result = await productCollection.deleteOne(query);
             res.json(result);
         })
+        // Get Api
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+
+
+        // add user 
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+        // Update user 
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
 
 
     }
@@ -129,9 +179,9 @@ async function run() {
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-    res.send('assignment server is running');
+    res.send('assignment 12 server is running');
 })
 
 app.listen(port, () => {
-    console.log('Server Running at port', port);
+    console.log('Server 12 Running at port', port);
 })
